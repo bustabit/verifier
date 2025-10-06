372e4fee88d7d89c8efe430e41319d7b7e821528
Leonardo Medina <133301211+leomedi@users.noreply.github.com>
2025-10-06 14:13:28 -0600

    add Vx Private Key

--- src/constants.ts
+++ src/constants.ts
@@ -7,7 +7,7 @@ export const TERMINATING_HASH = "567a98370fb7545137ddb53687723cf0b8a1f5e93b1f76f
 // from bustabit's third seeding event: https://bitcointalk.org/index.php?topic=5485695
 export const VX_GAME_SALT = "000000000000000000011f6e135efe67d7463dfe7bb955663ef88b1243b2deea";
 export const VX_PUB_KEY = "b40c94495f6e6e73619aeb54ec2fc84c5333f7a88ace82923946fc5b6c8635b08f9130888dd96e1749a1d5aab00020e4";
-export const VX_PRIV_KEY = ""; // TODO
+export const VX_PRIV_KEY = "1f0373e57723f4b862f2b9bd1279c9c6dcea73469fce6dcab9524b609c9c60f4";
 export const VX_LAST_GAME = 12279450;
 
 // from bustabit's second seeding event: https://bitcointalk.org/index.php?topic=2807542
